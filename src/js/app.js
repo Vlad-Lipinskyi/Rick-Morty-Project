ffc1376fe84d4f262e37ac607a8650cfe128473d
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2025-06-22 00:38:34 -0400

    ss

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import { getCharactersAPI } from "./api/characters-api";
 import { makeMarkUp } from "./index/make-markup";
 import { getEpisodesAPI } from "./api/episodes-api";
-import { makeMarkUpEpisodes } from "./episodes/make-markup-episdoes";
+// import { makeMarkUpEpisodes } from "./episodes/make-markup-episdoes";
+import "./episodes/filter-episodes"
 const charactersList = document.querySelector(".characters__js-fix")
 const episodesList = document.querySelector(".episodes__list-js");
  console.log(charactersList)
@@ -9,12 +10,9 @@ getCharactersAPI().then((data) => {
    const layout = makeMarkUp(data.results)
    console.log(layout)
   charactersList.innerHTML = layout;
-//   console.log(data);
-//   console.log(charactersList);
 });
 
 getEpisodesAPI().then((data) => {
   episodesList.innerHTML = makeMarkUpEpisodes(data.results);
-//   console.log(data);
-//   console.log(episodesList);
 });
+
---
+++ src/js/episodes/filter-episodes.js
@@ -0,0 +1,37 @@
+import { getEpisodesAPI } from "../api/episodes-api.js";
+import { makeMarkUpEpisodes } from "./make-markup-episdoes.js";
+
+const episodesList = document.querySelector(".episodes__list-js");
+const seasonSelect = document.querySelector("#episodes-select");
+
+let allEpisodes = [];
+
+const filterEpisodesBySeason = (seasonValue) => {
+  if (!seasonValue) {
+    return allEpisodes;
+  }
+  const seasonNumber = {
+    "first-season": "1",
+    "second-season": "2",
+    "third-season": "3",
+    "fourth-season": "4",
+    "fifth-season": "5",
+    "sixth-season": "6",
+  }[seasonValue];
+
+  return allEpisodes.filter((episode) => {
+    const seasonFromEpisodeCode = episode.episode.slice(2, 3); 
+    return seasonFromEpisodeCode === seasonNumber;
+  });
+};
+
+seasonSelect.addEventListener("change", (e) => {
+  const selectedSeason = e.target.value;
+  const filteredEpisodes = filterEpisodesBySeason(selectedSeason);
+  episodesList.innerHTML = makeMarkUpEpisodes(filteredEpisodes);
+});
+
+getEpisodesAPI().then((data) => {
+  allEpisodes = data.results;
+  episodesList.innerHTML = makeMarkUpEpisodes(allEpisodes);
+});
